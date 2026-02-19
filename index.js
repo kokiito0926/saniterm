be84e23a3af69225c01e506c32bd4e87436ba9cb
Koki Ito <kokiito0926@gmail.com>
2026-02-19 17:53:21 +0900

    feat: 🎸 プロトタイプを実装した

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import { stdin, argv } from "zx";
+import wrapAnsi from "wrap-ansi";
+import striptags from "striptags";
+import stripAnsi from "strip-ansi";
+import { decode } from "html-entities";
+
+if (process.stdin.isTTY) {
+	process.exit(1);
+}
+
+let text = await stdin();
+if (!text) {
+	process.exit(1);
+}
+
+const columns = argv?.columns || 80;
+// const width = argv?.width || process.stdout.columns || 80;
+
+text = striptags(text);
+
+text = decode(text);
+
+text = stripAnsi(text);
+
+text = text.replace(/[\x00-\x09\x0B\x0C\x0E-\x1F\x7F]/g, "");
+
+text = wrapAnsi(text, columns, { hard: true });
+
+console.log(text);
