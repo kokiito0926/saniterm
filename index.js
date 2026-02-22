39f0781cdb4e6b16da8d1ef345502697703a2fed
Koki Ito <kokiito0926@gmail.com>
2026-02-22 10:33:40 +0900

    feat: 🎸 コマンドライン引数のカラムの値をもとにして、折り返しを制御する

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ if (!text) {
 	process.exit(1);
 }
 
-const columns = argv?.columns || 80;
-// const width = argv?.width || process.stdout.columns || 80;
+const columns = argv?.columns || 0;
+// const columns = argv?.columns || process.stdout.columns || 80;
 
 text = stripAnsi(text);
 
 text = text.replace(/[\x00-\x09\x0B\x0C\x0E-\x1F\x7F]/g, "");
 
-text = wrapAnsi(text, columns, { hard: true });
+if (columns > 0) {
+	text = wrapAnsi(text, columns, { hard: true });
+}
 
 console.log(text);
